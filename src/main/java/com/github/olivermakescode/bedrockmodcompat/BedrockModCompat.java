5db4a58956aa9a03a638e2bcd21a0b59bf982134
Oliver-makes-code <59418903+Oliver-makes-code@users.noreply.github.com>
2021-08-08 11:08:49 -0500

    Setup

---
+++ src/main/java/com/github/olivermakescode/bedrockmodcompat/BedrockModCompat.java
@@ -0,0 +1,9 @@
+package com.github.olivermakescode.bedrockmodcompat;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BedrockModCompat implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}

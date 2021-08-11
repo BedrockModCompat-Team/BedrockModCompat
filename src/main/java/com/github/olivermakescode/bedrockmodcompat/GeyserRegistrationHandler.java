f85366a19606ebee00c0fb171436fe8de0dd4f4f
Oliver-makes-code <59418903+Oliver-makes-code@users.noreply.github.com>
2021-08-11 12:17:19 -0500

    Rename GeyserRegistrationEvents to GeyserRegistrationHandler

--- src/main/java/com/github/olivermakescode/bedrockmodcompat/GeyserRegistrationHandler.java
+++ src/main/java/com/github/olivermakescode/bedrockmodcompat/GeyserRegistrationHandler.java
@@ -3,15 +3,15 @@ package com.github.olivermakescode.bedrockmodcompat;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 
-public class GeyserRegistrationEvents {
+public class GeyserRegistrationHandler {
     public static MinecraftServer server;
 
     public static void register() {
         ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-            GeyserRegistrationEvents.server = server;
+            GeyserRegistrationHandler.server = server;
         });
         ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
-            GeyserRegistrationEvents.server = null;
+            GeyserRegistrationHandler.server = null;
         });
     }
 }

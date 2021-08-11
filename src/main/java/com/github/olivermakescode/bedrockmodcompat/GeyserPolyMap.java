284e265bb80f435bf6a932a949c8fe382fef4e2b
Oliver-makes-code <59418903+Oliver-makes-code@users.noreply.github.com>
2021-08-11 15:33:00 -0500

    Generate polys specifically for bedrock; for chest GUIs

---
+++ src/main/java/com/github/olivermakescode/bedrockmodcompat/GeyserPolyMap.java
@@ -0,0 +1,71 @@
+package com.github.olivermakescode.bedrockmodcompat;
+
+import com.google.common.collect.ImmutableMap;
+import io.github.theepicblock.polymc.api.PolyMap;
+import io.github.theepicblock.polymc.api.block.BlockPoly;
+import io.github.theepicblock.polymc.api.gui.GuiPoly;
+import io.github.theepicblock.polymc.api.item.ItemPoly;
+import io.github.theepicblock.polymc.impl.Util;
+import io.github.theepicblock.polymc.impl.poly.gui.NaiveStackListingChestPoly;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+
+public class GeyserPolyMap implements PolyMap {
+    public HashMap<ScreenHandlerType<?>, GuiPoly> guiPolys;
+
+    public GeyserPolyMap() {
+        guiPolys = new HashMap<>();
+        for (ScreenHandlerType<?> gui : Registry.SCREEN_HANDLER)
+            if (!Util.isVanilla(Registry.SCREEN_HANDLER.getId(gui)))
+                guiPolys.put(gui, new NaiveStackListingChestPoly());
+    }
+    @Override
+    public ItemStack getClientItem(ItemStack serverItem, @Nullable ServerPlayerEntity player) {
+        return serverItem;
+    }
+
+    @Override
+    public BlockState getClientBlock(BlockState serverBlock) {
+        return serverBlock;
+    }
+
+    @Override
+    public GuiPoly getGuiPoly(ScreenHandlerType<?> serverGuiType) {
+        return guiPolys.get(serverGuiType);
+    }
+
+    @Override
+    public BlockPoly getBlockPoly(Block block) {
+        return null;
+    }
+
+    @Override
+    public ImmutableMap<Item, ItemPoly> getItemPolys() {
+        return null;
+    }
+
+    @Override
+    public ImmutableMap<Block, BlockPoly> getBlockPolys() {
+        return null;
+    }
+
+    @Override
+    public ItemStack reverseClientItem(ItemStack clientItem) {
+        return clientItem;
+    }
+
+    @Override
+    public boolean isVanillaLikeMap() {
+        return false;
+    }
+
+}
--- src/main/java/com/github/olivermakescode/bedrockmodcompat/GeyserRegistrationHandler.java
+++ src/main/java/com/github/olivermakescode/bedrockmodcompat/GeyserRegistrationHandler.java
@@ -1,10 +1,14 @@
 package com.github.olivermakescode.bedrockmodcompat;
 
+import io.github.theepicblock.polymc.api.PolyMap;
+import io.github.theepicblock.polymc.api.misc.PolyMapProvider;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
+import org.geysermc.connector.GeyserConnector;
 
 public class GeyserRegistrationHandler {
     public static MinecraftServer server;
+    public static PolyMap GEYSERMAP = new GeyserPolyMap();
 
     public static void register() {
         ServerLifecycleEvents.SERVER_STARTING.register(server -> {
@@ -13,5 +17,7 @@ public class GeyserRegistrationHandler {
         ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
             GeyserRegistrationHandler.server = null;
         });
+
+        PolyMapProvider.EVENT.register(player -> GeyserConnector.getInstance().getPlayerByUuid(player.getUuid()) != null ? GEYSERMAP : null);
     }
 }

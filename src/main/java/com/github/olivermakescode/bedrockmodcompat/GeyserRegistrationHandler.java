4b259611eab5be0e9f32274ef81cd5bd956eac50
Ryan <redryma@icloud.com>
2021-08-25 21:35:32 -0400

    Updates

--- src/main/java/com/github/olivermakescode/bedrockmodcompat/GeyserRegistrationHandler.java
+++ src/main/java/com/github/olivermakescode/bedrockmodcompat/GeyserRegistrationHandler.java
@@ -3,8 +3,12 @@ package com.github.olivermakescode.bedrockmodcompat;
 import io.github.theepicblock.polymc.api.PolyMap;
 import io.github.theepicblock.polymc.api.misc.PolyMapProvider;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.item.Item;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.registry.Registry;
 import org.geysermc.connector.GeyserConnector;
+import org.geysermc.connector.registry.populator.ItemRegistryPopulator;
+import org.lwjgl.system.CallbackI;
 
 public class GeyserRegistrationHandler {
     public static MinecraftServer server;
@@ -19,5 +23,13 @@ public class GeyserRegistrationHandler {
         });
 
         PolyMapProvider.EVENT.register(player -> GeyserConnector.getInstance().getPlayerByUuid(player.getUuid()) != null ? GEYSERMAP : null);
+
+        registerItemMappings();
+    }
+
+    public static void registerItemMappings() {
+        for (int i = 0; i <= Registry.ITEM.stream().count(); i++) {
+            ItemRegistryPopulator.externalItemRegisters.add(Registry.ITEM.getId(Registry.ITEM.get(i)).getNamespace() + ":" + Registry.ITEM.getId(Registry.ITEM.get(i)).getPath());
+        }
     }
 }
